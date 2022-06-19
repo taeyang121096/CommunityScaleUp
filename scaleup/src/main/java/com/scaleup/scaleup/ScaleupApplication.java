21fd039c89aef081bd335809c4d106524c3deb68
taeyang121096 <taeyang121096@naver.com>
2022-06-20 02:12:20 +0900

    Project init Spring Boot and Jsp

---
+++ scaleup/src/main/java/com/scaleup/scaleup/ScaleupApplication.java
@@ -0,0 +1,13 @@
+package com.scaleup.scaleup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaleupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScaleupApplication.class, args);
+	}
+
+}
