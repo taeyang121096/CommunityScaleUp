5902a4e033b0d1d7f117ce6f71aabe98b2c7f839
taeyang121096 <taeyang121096@naver.com>
2022-06-21 01:36:38 +0900

    init

---
+++ scaleup/src/test/java/com/scaleup/scaleup/ScaleupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaleup.scaleup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaleupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
