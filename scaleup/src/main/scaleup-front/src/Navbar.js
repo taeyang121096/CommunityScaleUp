ce0e84b9232003e35dbfc3c339711cd7a7bb7b40
jinju <jinju000202@naver.com>
2022-07-09 22:20:52 +0900

    dev, upd page

--- scaleup/src/main/scaleup-front/src/Navbar.js
+++ scaleup/src/main/scaleup-front/src/Navbar.js
@@ -1,58 +1,30 @@
-import React, { useState, useEffect} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import {Button} from './Button';
 import './Navbar.css';
 
-
 function Navbar() {
-
-    const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
-
-
-    const handleClick = () => setClick(!click) ;
-    const closeMobileMenu = () => setClick(false);
-
-    // 화면 크기에 따라서 버튼이 보이고 안보이도록 설정한다. 
-    const showButton = () => {
-        if(window.innerWidth <= 960){
-            setButton(false)
-        }
-        else {
-            setButton(true);
-        }
-    };
-
-    window.addEventListener('resize', showButton);
-
-     
-   
     return (
         <>
-        <nav className = 'navbar'>
-            <div className = 'navbar-container'>
-                {/* 모바일버전에서 클릭하면 메뉴 보이도록 설정하는 것도 한다. (close Mobile Menu)는 다시 버튼 누르면 없어지고 생기고 하도록 한다.  */}
-                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+        <div className='navbar'>
+            <div className='navbar-container'>
+                <Link to='/' className = 'navbar-logo' >
                     BAMDAL 
                 </Link>
-                <div className='menu-icon' onClick={handleClick}>
-                    <i className = {click ? 'fas fa-times' : 'fas fa-bars' } />
-                </div>
-                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <ul className= 'nav-menu'>
                     <li className='nav-item'>
-                        <Link to='/sign-up' className='nav-links' onClick = {closeMobileMenu}>
+                        <Link to='/sign-up' className='nav-links' >
                             SIGN UP
                         </Link>
                     </li>     
                     <li className='nav-item'>
-                        <Link to='/sign-in' className='nav-links' onClick = {closeMobileMenu}>
+                        <Link to='/sign-in' className='nav-links' >
                            SIGN IN
-                            <i className="fa fa-user" aria-hidden="true"/>
+                           <i className="fa fa-user" />{/*아이콘 */}
                         </Link>                 
                     </li>
                 </ul>
             </div>
-        </nav>
+        </div>
         </>
     );
 }
