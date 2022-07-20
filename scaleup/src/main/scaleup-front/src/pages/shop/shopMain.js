64778808dca8a361aa1ddb1f463d05436a784f2c
gpqls <tnqls1035@kyonggi.ac.kr>
2022-07-20 23:31:05 +0900

    dev : shopMain

--- scaleup/src/main/scaleup-front/src/pages/shop/shopMain.js
+++ scaleup/src/main/scaleup-front/src/pages/shop/shopMain.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import {HashRouter, BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import ShopMenu from './shopMenu';
 import ShopNavbar from './shopNavbar';
 import '../../styles/shop/shopMain.css'
 
@@ -8,18 +7,31 @@ function ShopMain() { //SHOP 메인 페이지
     return(
         <>
         <ShopNavbar/>
-        <div className='shop-container'>
-            <div>
-                <h2>밤달샵</h2>
-                <div>
-                    사진 4개
+        <div className='shop-main'>
+            <div className='shop-container'>
+            <div className='adv'>
+                <h1 className='adv-title'>밤달샵</h1>
+                <div className='adv-img'>
+                    <img src='https://via.placeholder.com/750x250'/>
                 </div>
             </div>
-            <div>
-                <h3>오늘의 할인</h3>
-                <div>
-                    사진 4개
+            <div className='discount-list'>
+                <h3 className='discount-title'>오늘의 할인</h3>
+                <div className='discount-img'>
+                    <div className='imglist'>
+                        <img src='https://via.placeholder.com/180x200'/>
+                    </div>
+                    <div className='imglist'>
+                        <img src='https://via.placeholder.com/180x200'/>
+                    </div>
+                    <div className='imglist'>
+                        <img src='https://via.placeholder.com/180x200'/>
+                    </div>
+                    <div className='imglist'>
+                        <img src='https://via.placeholder.com/180x200'/>
+                    </div>
                 </div>
+            </div>
             </div>
         </div>
         </>
