348a9b4d795cd567e33231db7dd04bad06a148aa
gpqls <tnqls1035@kyonggi.ac.kr>
2022-07-21 13:32:07 +0900

    upd : Acc dev : Sidebar

--- scaleup/src/main/scaleup-front/src/pages/shop/Acc.js
+++ scaleup/src/main/scaleup-front/src/pages/shop/Acc.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {HashRouter, BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import ShopNavbar from './shopNavbar';
+import Sidebar from './Sidebar';
 import '../../styles/shop/Acc.css'
 
 function Acc(){
@@ -8,10 +9,11 @@ function Acc(){
         <>
         <ShopNavbar/>
         <div className='acc-main'>
+        <Sidebar/>
             <div className='acc-container'>
             <h1 className='acc-title'>액세서리</h1>
                 <div className='best'>
-                    <h2 className='best-title'>오늘의 BEST</h2>
+                <h2 className='best-title'>오늘의 BEST</h2>
                     <div className='best-img'>
                     <div className='imglist'>
                         <img src='https://via.placeholder.com/180x200'/>
@@ -60,11 +62,8 @@ function Acc(){
                         29000원
                     </div>
                 </div>
-
                 </div>
-
             </div>
-
         </div>
         </>
 
