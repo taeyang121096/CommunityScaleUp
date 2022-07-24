58dbe9d347b9a8e082c928695de4715154d810ec
gpqls <tnqls1035@kyonggi.ac.kr>
2022-07-25 00:26:46 +0900

    upd : link

--- scaleup/src/main/scaleup-front/src/App.js
+++ scaleup/src/main/scaleup-front/src/App.js
@@ -8,12 +8,22 @@ import Acc from './pages/shop/Acc';
 import Clothes from './pages/shop/Clothes';
 import ShopMain from './pages/shop/shopMain';
 import CoummunityMain from './pages/community/communityMain';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <>
     <Router>
-      <CoummunityMain/>
+    <Routes>
+                <Route path="/" exact={true} element = {<MainPage/>}/>
+                <Route path="/sign-up" element = {<Signup/>}/>
+                <Route path="/shop" element = {<ShopMain/>}/>
+                <Route path="/community" element = {<CoummunityMain/>}/>
+                <Route path="/acc" element = {<Acc/>}/>
+                <Route path="/toy" element = {<Toy/>}/>
+                <Route path="/food" element = {<Food/>}/>
+                <Route path="/clothes" element = {<Clothes/>}/>
+        </Routes>
       </Router>
     </>
   );
