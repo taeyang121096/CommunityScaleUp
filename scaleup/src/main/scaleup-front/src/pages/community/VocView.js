f6798c4f3e53f8775ca5dedc0b8d3be9ed8b63a4
Alice <gogoalice@naver.com>
2022-09-13 14:46:30 +0900

    upd : VocView

--- scaleup/src/main/scaleup-front/src/pages/community/VocView.js
+++ scaleup/src/main/scaleup-front/src/pages/community/VocView.js
@@ -11,25 +11,20 @@ function VocView(){ //글쓰기 상세페이지 왜 안올라가지??
         <h2 align="center">게시글 상세정보</h2>
         <div className="voc-view-wrapper">
             <div className="voc-view-row">
-                <label>게시글 번호</label>
+                <label for="voc-number">게시글 번호</label>
                 
             </div>
             <div className="voc-view-row">
-                <label>제목</label>
+                <label for="voc-title">제목</label>
                 
             </div>
             <div className="voc-view-row">
-                <label>작성일</label>
+                <label for="voc-date"> 작성일</label>
                 
             </div>
             <div className="voc-view-row">
-                <label>내용</label>
-                <div>
-                {
-                어쩌구저쩌구
-
-                }
-                </div>
+                <label for="voc-detail">내용</label>
+                <div> 내용 들어감 어쩌구 저쩌구</div>
             </div>
         </div></>
 
